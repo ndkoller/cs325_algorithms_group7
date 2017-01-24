32b199f6df4ce73fee274fa2715374bb4c9a0c2e
tehKruser <krusej@oregonstate.edu>
2017-01-23 22:32:30 -0700

    Enumeration: Alg and Alg2 added
    Enumeration and Better Enumeration algorithms added

--- Project_1/project1_enumeration.py
+++ Project_1/project1_enumeration.py
@@ -5,12 +5,31 @@ from random import *
 
 # File names
 data_file = 'MSS_Problems.txt'
-results_file = 'MSS_TestResults1.txt'
-exp_results_file = 'MS_ExperimentalResults.txt'
+results_file = 'MSS_Results1.txt'
+exp_results_file = 'MS_ExperimentalResults1.txt'
 
 
 # Algorithm 1: Enumeration
 def MAXSUBARRAY_Enum(array):
+    n = len(array)
+    max_sum = -1
+    max_i = 0
+    max_j = 0
+    for i in range(n):
+        sa_sum = 0
+        for j in range(i, n):
+            for ea in array[i:j+1]:
+                sa_sum += int(ea)
+            if sa_sum > max_sum:
+                max_sum = sa_sum
+                max_i = i
+                max_j = j
+    return array[max_i:max_j+1], max_sum
+
+
+
+# Algorithm 2: Better Enumeration
+def MAXSUBARRAY_BetterEnum(array):
     n = len(array)
     max_sum = -1
     max_i = 0
@@ -29,8 +48,6 @@ def MAXSUBARRAY_Enum(array):
 # Delete contents of test results file
 with open(results_file, 'w') as fw:
     fw.write("Algorithm 1: Enumeration Experimental Run Times\n")
-
-print("Algorithm 1: Enumeration Experimental Run Times")
 
 
 # Open data_file
@@ -56,19 +73,27 @@ with open(data_file, 'r') as fr:
             fw.write(str(maxsum) + "\n\n")
 
 
-# Experimental Analysis
+######## Experimental Time Runs ########
 
 # Delete contents of test results file
 with open(exp_results_file, 'w') as fw:
-    fw.write("")
+    fw.write("Experimental Time Runs\n")
+print("Experimental Time Runs")
 
 # Array for N sizes
-N = [50, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000]
+N = [25, 35, 50, 75, 100, 125, 250, 500, 750, 1000]
+
+# Algorithm 1: Enumeration
+with open(exp_results_file, 'a') as fw:
+    fw.write("\nAlgorithm 1: Enumeration\n")
+print("\nAlgorithm 1: Enumeration")
 
 for ea in N:
     # Create array of random integers of size N
     array = []
-    for i in range(ea):
+    n_multiplier = 1
+    N_adjusted = int(ea) * n_multiplier
+    for i in range(N_adjusted):
         array.append(randint(-100, 101))
 
     # Start timer
@@ -83,3 +108,30 @@ for ea in N:
     with open(exp_results_file, 'a') as fw:
         fw.write("N = " + str(ea) + " | Time = " + str(totalTime.total_seconds()) + "\n")
     print("N =", ea, "| Time =", totalTime.total_seconds())
+
+
+# Algorithm 2: Better Enumeration
+with open(exp_results_file, 'a') as fw:
+    fw.write("\nAlgorithm 2: Better Enumeration\n")
+print("\nAlgorithm 2: Better Enumeration")
+
+for ea in N:
+    # Create array of random integers of size N
+    array = []
+    n_multiplier = 10
+    N_adjusted = int(ea) * n_multiplier
+    for i in range(N_adjusted):
+        array.append(randint(-100, 101))
+
+    # Start timer
+    startTime = datetime.now()
+
+    # Call Algorithm 1: Enumeration to get the max sub array and max sum
+    subarray, maxsum = MAXSUBARRAY_BetterEnum(array)
+
+    # stop timer
+    totalTime = datetime.now() - startTime
+
+    with open(exp_results_file, 'a') as fw:
+        fw.write("N = " + str(N_adjusted) + " | Time = " + str(totalTime.total_seconds()) + "\n")
+    print("N =", N_adjusted, "| Time =", totalTime.total_seconds())
