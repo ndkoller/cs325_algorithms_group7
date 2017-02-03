bc68a55388bd8bdec0ba8efe4baae0ce08f1ad41
tehKruser <krusej@oregonstate.edu>
2017-02-02 23:29:54 -0700

    Alg 1 update
    - added recursive algorithm 1
    - general clean up of code
    - removed PyCharm folder - for local compiling

--- Project_2/p2algorithms.py
+++ Project_2/p2algorithms.py
@@ -1,14 +1,37 @@
 # Algorithm 1: Enumeration
-def ChangeSlow(array, target):
-    target = int(target)
-    c = [0] * len(array)
-    m = 0
+# Helper function
+def change(target, coins, coins_list):
+    if sum(coins_list) == target:
+        yield coins_list
+    elif sum(coins_list) > target:
+        pass
+    elif coins == []:
+        pass
+    else:
+        for c in change(target, coins[:], coins_list + [coins[0]]):
+            yield c
+        for c in change(target, coins[1:], coins_list):
+            yield c
+
+def ChangeSlow(coins, target):
+    # Call helper function and create list of all solutions
+    solutions = [s for s in change(target, coins, [])]
+
+    # optimal solution
+    opt = min(solutions, key=len)
+    m = len(opt)
+
+    c = [0] * len(coins)
+    i = 0
+    for ea in coins:
+        c[i] = opt.count(ea)
+        i += 1
+
     return c, m
 
 
 # Algorithm 2: Change Greedy
 def ChangeGreedy(array, target):
-    #target = int(target)
     c = [0] * len(array)
     m = 0
     i = len(array) - 1
