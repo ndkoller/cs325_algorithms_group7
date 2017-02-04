3fab05255f595cc56a99322b365404846058ddaa
tehKruser <krusej@oregonstate.edu>
2017-02-04 15:19:42 -0700

    Gold Update
    Code complete

--- Project_2/project2.py
+++ Project_2/project2.py
@@ -4,24 +4,25 @@ from random import *
 import p2algorithms as algs
 from itertools import zip_longest
 import sys
-import math
 
 # File names
 if len(sys.argv) == 1:
-    data_file = 'correctnessTest.txt'         # for testing purposes
+    data_file = 'correctnessTest.txt'  # for testing purposes
 else:
     data_file = str(sys.argv[1])
 results_file = data_file[:-4] + 'change.txt'
 PR3_exp_results_file = 'PR3_ExperimentalResults.txt'
+PR4_exp_results_file = 'PR4_ExperimentalResults.txt'
+PR5_exp_results_file = 'PR5_ExperimentalResults.txt'
 PR7_exp_results_file = 'PR7_ExperimentalResults.txt'
 
 # Turn runs on/off by setting to True/False
 run_file = True
 run_pr3 = True
-run_pr4 = False
-run_pr5 = False
-run_pr6 = False
+run_pr4 = True
+run_pr5 = True
 run_pr7 = True
+
 
 # Python Documentation Recipe to group file input
 def GROUPER(iterable, n, fillvalue=None):
@@ -48,13 +49,12 @@ def RUNEXPERIMENT(alg, n_array, data, n_multiplier, print_header, output_file):
 
     if print_header:
         with open(output_file, 'a') as fw:
-            fw.write("N Values\tTime (Seconds)\tOptimal Solution\tDenominations\n")
-        print(template.format("N Values", "Time (s)", "Optimal Solution", "Denominations"))
+            fw.write("A Values\tTime (Seconds)\tOptimal Solution\tDenominations\n")
+        print(template.format("A Values", "Time (s)", "Optimal Solution", "Denominations"))
 
     for ea in n_array:
-        
         target = ea * n_multiplier
-        
+
         # Start timer
         startTime = datetime.now()
 
@@ -68,9 +68,14 @@ def RUNEXPERIMENT(alg, n_array, data, n_multiplier, print_header, output_file):
             fw.write(str(target) + "\t" + str(totalTime.total_seconds()) + "\t" + str(m) + "\t" + str(len(data)) + "\n")
         data_print = [target, totalTime.total_seconds(), m, len(data)]
         print(template.format(*data_print))
-    
 
-######## Run Algorithms on Input File ########
+
+'''
+########################################################
+############# Run Algorithms on Input File #############
+########################################################
+'''
+
 if run_file:
     print("\n******Solving Input File ******\nRunning Solutions...\n")
     # Delete contents of test results file
@@ -81,8 +86,7 @@ if run_file:
     with open(data_file, 'r') as fr:
 
         # Read every 2 lines using grouper function
-        for line1, line2 in GROUPER(fr,2):
-
+        for line1, line2 in GROUPER(fr, 2):
             # Split line into an array of values
             # This is the denomination array in test files
             data = list(map(int, line1.split()))
@@ -90,7 +94,6 @@ if run_file:
 
             # Append Enumeration results to 'results_file'
             with open(results_file, 'a') as fw:
-
                 # Call Algorithm 1: ChangeSlow to get the max sub array and max sum
                 c, m = algs.ChangeSlow(data, dataTarget)
                 print("Algorithm 1: Change Slow\nc:", c, 'm:', m)
@@ -112,42 +115,22 @@ if run_file:
                 fw.write("Algorithm 3: Change DP\n")
                 WRITERESULTS(data, c, m, fw)
 
-
-######## Experimental Time Runs ########
-
-# Array for Project Report Question 3
-N = []
-i = 100
-for i in range(100, 305, 5):
-    N.append(i)
-VN = [1, 5, 10, 25, 50]
-
-# Alternative Arrays for Project Report Question 4
-M = []
-j = 2000
-for j in range(2000, 2201, 1):
-    M.append(j)
-VM1 = [1, 2, 6, 12, 24, 48, 60]
-VM2 = [1, 6, 13, 37, 150]
-j = 10000
-ML = []
-for j in range(10000, 10101, 1):
-    ML.append(j)
-    
-# Array for Project Report Question 5 (Re-Use M's from 4)
-F = [1]
-k = 2
-for k in range(2, 32, 2):
-    F.append(k)
-
-# Question 7 Number of Denominations vs Time
-den_array = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
-target = [75]
-
-   
+'''
+########################################################
+################ Experimental Time Runs ################
+########################################################
+'''
 
 '''********************** Question 3 Runs **********************'''
 if run_pr3:
+
+    # Array for Project Report Question 3
+    A3 = []
+    i = 100
+    for i in range(100, 305, 5):
+        A3.append(i)
+    V3 = [1, 5, 10, 25, 50]
+
     # Delete contents of test results file
     with open(PR3_exp_results_file, 'w') as fw:
         fw.write("Experimental Time Runs\n")
@@ -158,76 +141,139 @@ if run_pr3:
         fw.write("\nAlgorithm 1: Change Slow\n")
     print("\nAlgorithm 1: Change Slow")
 
-    RUNEXPERIMENT(algs.ChangeSlow, N, VN, 1, True, PR3_exp_results_file)
+    RUNEXPERIMENT(algs.ChangeSlow, A3, V3, 1, True, PR3_exp_results_file)
 
     # Algorithm 2: Change Greedy
     with open(PR3_exp_results_file, 'a') as fw:
         fw.write("\nAlgorithm 2: Change Greedy\n")
     print("\nAlgorithm 2: Change Greedy")
 
-    RUNEXPERIMENT(algs.ChangeGreedy, N, VN, 100000, True, PR3_exp_results_file)
+    RUNEXPERIMENT(algs.ChangeGreedy, A3, V3, 100000, True, PR3_exp_results_file)
 
     # Algorithm 3: Change DP
     with open(PR3_exp_results_file, 'a') as fw:
         fw.write("\nAlgorithm 3: Change Dynamic Programming\n")
     print("\nAlgorithm 2: Change Dynamic Programming")
 
-    RUNEXPERIMENT(algs.ChangeDP, N, VN, 1000, True, PR3_exp_results_file)
+    RUNEXPERIMENT(algs.ChangeDP, A3, V3, 1000, True, PR3_exp_results_file)
 
+'''********************** Question 4 Runs **********************'''
+# Arrays used for questions 4 and 5
+A_small = []
+for j in range(25, 126, 1):
+    A_small.append(j)
 
-# Question 4 - VM1
+A_medium = []
+for j in range(100000, 100101, 1):
+    A_medium.append(j)
 
-# # Algorithm 1: Change Slow
-# with open(exp_results_file, 'a') as fw:
-#    fw.write("\nAlgorithm 1: Change Slow\n")
-# print("\nAlgorithm 1: Change Slow")
+A_large = []
+for j in range(10000000, 10000101, 1):
+    A_large.append(j)
 
-# RUNEXPERIMENT(algs.ChangeSlow, M, VM1, 1, exp_results_file)
+if run_pr4:
+    # Arrays for Project Report Question 4
+    V4_1 = [1, 2, 6, 12, 24, 48, 60]
+    V4_2 = [1, 6, 13, 37, 150]
 
-# # Algorithm 2: Change Greedy
-# with open(exp_results_file, 'a') as fw:
-#    fw.write("\nAlgorithm 2: Change Greedy\n")
-# print("\nAlgorithm 2: Change Greedy")
+    # Delete contents of test results file
+    with open(PR4_exp_results_file, 'w') as fw:
+        fw.write("Experimental Time Runs\n")
+    print("\n******Project Report Question 4******\nRunning Experiment...")
 
-# RUNEXPERIMENT(algs.ChangeGreedy, M, VM1, 1, exp_results_file)
+    '''***************** Solutions for A when V1 Denominations *****************'''
 
-# # Algorithm 3: Change DP
-# # with open(exp_results_file, 'a') as fw:
-# #     fw.write("\nAlgorithm 3: Change DP\n")
-# # print("\nAlgorithm 3: Change DP")
-# # Using ML Targets
-# # RUNEXPERIMENT(algs.ChangeDP, ML, VM1, 1, exp_results_file)
+    # Algorithm 1: Change Slow (V1)
+    with open(PR4_exp_results_file, 'a') as fw:
+       fw.write("\nAlgorithm 1: Change Slow (V1)\n")
+    print("\nAlgorithm 1: Change Slow (V1)")
 
-# # Question 4 - VM2
+    RUNEXPERIMENT(algs.ChangeSlow, A_small, V4_1, 1, True, PR4_exp_results_file)
 
-# # Algorithm 1: Change Slow
-# with open(exp_results_file, 'a') as fw:
-#    fw.write("\nAlgorithm 1: Change Slow\n")
-# print("\nAlgorithm 1: Change Slow")
+    # Algorithm 2: Change Greedy (V1)
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 2: Change Greedy (V1)\n")
+    print("\nAlgorithm 2: Change Greedy (V1)")
 
-# RUNEXPERIMENT(algs.ChangeSlow, M, VM2, 1, exp_results_file)
+    RUNEXPERIMENT(algs.ChangeGreedy, A_large, V4_1, 1, True, PR4_exp_results_file)
 
-# # Algorithm 2: Change Greedy
-# with open(exp_results_file, 'a') as fw:
-#    fw.write("\nAlgorithm 2: Change Greedy\n")
-# print("\nAlgorithm 2: Change Greedy")
+    # Algorithm 3: Change DP (V1)
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 3: Change DP (V1)\n")
+    print("\nAlgorithm 3: Change DP (V1)")
 
-# RUNEXPERIMENT(algs.ChangeGreedy, M, VM2, 1, exp_results_file)
+    RUNEXPERIMENT(algs.ChangeDP, A_medium, V4_1, 1, True, PR4_exp_results_file)
 
-# # Algorithm 3: Change DP
-# # with open(exp_results_file, 'a') as fw:
-#       fw.write("\nAlgorithm 3: Change DP\n")
-# # print("\nAlgorithm 3: Change DP")
+    '''***************** Solutions for A when V2 Denominations *****************'''
 
-# # RUNEXPERIMENT(algs.ChangeDP, ML, VM2, 1, exp_results_file)
+    # Algorithm 1: Change Slow (V2)
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 1: Change Slow (V2)\n")
+    print("\nAlgorithm 1: Change Slow (V2)")
+
+    RUNEXPERIMENT(algs.ChangeSlow, A_small, V4_2, 1, True, PR4_exp_results_file)
+
+    # Algorithm 2: Change Greedy (V2)
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 2: Change Greedy (V2)\n")
+    print("\nAlgorithm 2: Change Greedy (V2)")
+
+    RUNEXPERIMENT(algs.ChangeGreedy, A_large, V4_2, 1, True, PR4_exp_results_file)
+
+    # Algorithm 3: Change DP (V1)
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 3: Change DP (V2)\n")
+    print("\nAlgorithm 3: Change DP (V2)")
+
+    RUNEXPERIMENT(algs.ChangeDP, A_medium, V4_2, 1, True, PR4_exp_results_file)
+
+'''********************** Question 5 Runs **********************'''
+if run_pr5:
+
+    # Array for Project Report Question 5 (Re-Use A's from 4)
+    V5 = [1]
+    for k in range(2, 32, 2):
+        V5.append(k)
+
+    # Delete contents of test results file
+    with open(PR4_exp_results_file, 'w') as fw:
+        fw.write("Experimental Time Runs\n")
+    print("\n******Project Report Question 5******\nRunning Experiment...")
+
+    # Algorithm 1: Change Slow
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 1: Change Slow\n")
+    print("\nAlgorithm 1: Change Slow")
+
+    RUNEXPERIMENT(algs.ChangeSlow, A_small[:50], V5, 1, True, PR5_exp_results_file)
+
+    # Algorithm 2: Change Greedy
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 2: Change Greedy\n")
+    print("\nAlgorithm 2: Change Greedy")
+
+    RUNEXPERIMENT(algs.ChangeGreedy, A_large, V5, 1, True, PR5_exp_results_file)
+
+    # Algorithm 3: Change DP
+    with open(PR4_exp_results_file, 'a') as fw:
+        fw.write("\nAlgorithm 3: Change DP\n")
+    print("\nAlgorithm 3: Change DP")
+
+    RUNEXPERIMENT(algs.ChangeDP, A_medium, V5, 1, True, PR5_exp_results_file)
+
 
 '''********************** Question 7 Runs **********************'''
 if run_pr7:
+
+    # Question 7 Number of Denominations vs Time
+    den_array = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    target = [75]
+
     with open(PR7_exp_results_file, 'w') as fw:
         fw.write("Question 7: Denominations versus Time\n")
     print("\n******Project Report Question 7******\nRunning Experiment...")
 
-    for i in range(3, len(den_array)-1, 1):
+    for i in range(3, len(den_array) - 1, 1):
         print_header = False
 
         if i == 3:
@@ -257,7 +303,7 @@ if run_pr7:
         if i == 3:
             print_header = True
 
-    # Algorithm 3: Change DP
+            # Algorithm 3: Change DP
         if print_header:
             with open(PR7_exp_results_file, 'a') as fw:
                 fw.write("\nAlgorithm 3: Change DP\n")
